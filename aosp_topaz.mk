17489f95ca81f0010cce1e793a89105793d6865d
Kizziama <kizziama@proton.me>
2024-02-04 18:25:57 +0000

    topaz: HorizonDroid Bringup Official
    Change-Id: Ic5f991e3a0248c1993eb2ed1cad217a2a6c80aad

--- aosp_topaz.mk
+++ aosp_topaz.mk
@@ -11,10 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from topaz device
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HorizonDroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_topaz
+# HorizonDroid
+TARGET_BOOT_ANIMATION_RES := 1080
+HORIZON_MAINTAINER := SuperXorn
+HORIZON_BUILD_TYPE := OFFICIAL
+
+# Innertune
+TARGET_BUILD_INNERTUNE := true
+
+PRODUCT_NAME := aosp_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
